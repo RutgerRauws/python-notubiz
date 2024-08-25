d8638065859efa0146e39174fd6928bff370ec8f
Rutger Rauws <rutgerrauws@gmail.com>
2024-08-25 12:08:58 +0200

    Add minimal Speakers api wrapper

---
+++ notubiz/api/__init__.py
@@ -0,0 +1,1 @@
+from notubiz.api.speakers import NotubizSpeakers
---
+++ notubiz/api/speakers.py
@@ -0,0 +1,78 @@
+from attrs import define
+import cattrs
+from cattrs import transform_error
+from cattrs.gen import make_dict_unstructure_fn, make_dict_structure_fn, override
+from typing import Optional
+
+from notubiz.api_client import ApiClient
+
+@define
+class SpeakerAttributes:
+    id: int
+    person_id: int
+    active: int
+    last_modified: str
+
+@define
+class Speaker:
+    photo: str
+    party: str
+    email: str
+    initials: str
+    firstname: str
+    lastname: str
+    sex: str
+    function: str
+    url: str
+    attributes: SpeakerAttributes
+
+@define
+class Speakers:
+    speakers: list[Speaker]
+
+class NotubizSpeakers:
+    api_client : ApiClient
+
+    def __init__(self, api_client : ApiClient):
+        self.api_client = api_client
+
+    # def find_by_speaker_id(self, speaker_id : int):
+    #     for speaker in self.speakers:
+    #         if speaker.attributes.id == speaker_id:
+    #             return speaker
+        
+    #     return None
+    
+    
+    # def find_by_person_id(self, person_id : int):
+    #     for speaker in self.speakers:
+    #         if speaker.attributes.person_id == person_id:
+    #             return speaker
+        
+    #     return None
+
+    def get_all(self):
+        json_object = self.api_client.get("speakers")
+        return get_notubiz_speakers_from_json(json_object)
+
+
+def get_notubiz_speakers_from_json(json_object : any):
+    c = cattrs.Converter()
+
+    unst_hook = make_dict_unstructure_fn(Speakers, c, speakers=override(rename="speaker"))
+    st_hook = make_dict_structure_fn(Speakers, c, speakers=override(rename="speaker"))
+    c.register_unstructure_hook(Speakers, unst_hook)
+    c.register_structure_hook(Speakers, st_hook)
+
+    unst_hook = make_dict_unstructure_fn(Speaker, c, attributes=override(rename="@attributes"))
+    st_hook = make_dict_structure_fn(Speaker, c, attributes=override(rename="@attributes"))
+    c.register_unstructure_hook(Speaker, unst_hook)
+    c.register_structure_hook(Speaker, st_hook)
+
+    try:
+        speakers = c.structure(json_object["speakers"], Speakers)
+    except Exception as exc:
+        print("\n".join(transform_error(exc)))
+        quit()
+
+    return speakers
