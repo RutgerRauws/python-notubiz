f1cfbf74700b379ebf6b8a28739340548b0e53b5
Rutger Rauws <rutgerrauws@gmail.com>
2025-01-02 19:52:05 +0100

    Enforce the use of Notubiz version 1.10.8

--- notubiz/configuration.py
+++ notubiz/configuration.py
@@ -3,4 +3,5 @@ from attrs import define
 @define
 class Configuration:
     organisation_id : int
+    api_version : str = "1.10.8" # To keep the used API stable
     base_url : str = "https://api.notubiz.nl/"
