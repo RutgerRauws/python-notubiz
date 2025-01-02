eba010a59c69084e321528d1b5546861a6314092
Rutger Rauws <rutgerrauws@gmail.com>
2025-01-02 15:44:49 +0100

    Add nested agenda items

--- notubiz/api/agenda_item.py
+++ notubiz/api/agenda_item.py
@@ -1,4 +1,4 @@
-from attrs import define
+from attrs import define, field
 import cattrs
 
 from datetime import datetime
@@ -15,17 +15,27 @@ class AgendaItem:
     start_date : Optional[datetime]
     end_date : Optional[datetime]
     is_heading : bool
+    agenda_items : list['AgendaItem'] = field(factory=list)
 
 
 def get_start_date(attributes) -> datetime:
-    return parse_date(get_attribute(attributes, 82))
+    try:
+        return parse_date(get_attribute(attributes, 82))
+    except Exception: # The nested agenda items seem to have no start dates.
+        return None
 
 def get_end_date(attributes) -> datetime:
-    return parse_date(get_attribute(attributes, 83))
-
+    try:
+        return parse_date(get_attribute(attributes, 83))
+    except Exception: # The nested agenda items seem to have no end dates.
+        return None
 
 def agenda_item_structure_hook(data: Dict[str, Any], cls: type) -> AgendaItem:
-    attributes = data["type_data"]["attributes"]
+    type_data = data.get("type_data", {})
+    attributes = type_data["attributes"]
+
+    # Use cattrs to structure the Meeting fields
+    agenda_items = NotubizAgendaItems.from_json(data["agenda_items"])
 
     return AgendaItem(
         id=data["id"],
@@ -34,7 +44,8 @@ def agenda_item_structure_hook(data: Dict[str, Any], cls: type) -> AgendaItem:
         description = get_description(attributes),
         start_date = get_start_date(attributes),
         end_date = get_end_date(attributes),
-        is_heading = data["type_data"]["heading"]
+        is_heading = data["type_data"]["heading"],
+        agenda_items = agenda_items
     )
 
 
@@ -45,7 +56,7 @@ class NotubizAgendaItems:
         c.register_structure_hook(datetime, lambda date_string, _: parse_date(date_string))
         c.register_structure_hook(AgendaItem, agenda_item_structure_hook)
 
-        agenda_items =  [c.structure(item, AgendaItem) for item in json_object]
+        agenda_items = [c.structure(item, AgendaItem) for item in json_object]
 
         return agenda_items
     
